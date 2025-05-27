3890eb5c6bedeba5e576b45b579f70b23b2c050e
yitzchak shaish <y350380350@gmail.com>
2025-05-27 10:12:26 +0300

    Added class "Commando"

---
+++ CommandoPproject/Commando.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandoPproject
+{
+    internal class Commando
+    {
+        string Name;
+        string CodeName;
+        List<string> Tools = new List<string> { "hammer", "chisel", "rope", "bag", "water bottle" };
+        string Status { get; set; }
+    
+    public Commando(string name, string codeName,  string status)
+        {
+            Name = name;
+            CodeName = codeName;
+            Status = status;
+        }
+        
+    public void Walk()
+        {
+            Status = "Walk";
+            Console.WriteLine($"The soldier : {Status}ed");
+        }
+        public void Hide()
+        {
+            Status = "Hide";
+            Console.WriteLine($"The soldier : {Status}ed");
+        }
+        public void Attack()
+        {
+            Console.WriteLine($"Commando with Code Name: \"{CodeName}\" is active.");
+        }
+
+    }
+
+}
