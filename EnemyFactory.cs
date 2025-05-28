9c2f1c37dc66dc087a908528ae8b5f8094831760
yitzchak shaish <y350380350@gmail.com>
2025-05-28 11:22:45 +0300

    Added class EnemyFactory, and functions GetEnemy  GetListEnemy

---
+++ EnemyFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CommandoPproject
+{
+    internal static class EnemyFactory
+    {
+        private static List<Enemy> listEnemy = new List<Enemy>();
+        private static List<string> namesEnemy = new List<string>
+        {
+            "mohamd", "aboo", "sinowar", "m2", "m3", "m5", "m6"
+        };
+
+        public static void GetEnemy(int num)
+        {
+            listEnemy.Clear(); 
+            for (int i = 0; i < num && i < namesEnemy.Count; i++)
+            {
+                listEnemy.Add(new Enemy(namesEnemy[i]));
+            }
+        }
+
+        public static List<Enemy> GetListEnemy()
+        {
+            return listEnemy;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,18 +10,23 @@ namespace CommandoPproject
     {
         static void Main(string[] args)
         {
-            Commando c1= new Commando("moshe","xxx", "Hide");
-            c1.Walk();
-            c1.SayName("COLONEL");
-            Weapon ww = new Weapon("gon","refael",3);
-            AirCommando a1 = new AirCommando("itzik", "231", " ");
-            SeaCommando s1 = new SeaCommando("chaim", "===", " ");
-            Commando [] nn = { c1, a1, s1 };
-            foreach (var i in nn)
+            //Commando c1= new Commando("moshe","xxx", "Hide");
+            //c1.Walk();
+            //c1.SayName("COLONEL");
+            //Weapon ww = new Weapon("gon","refael",3);
+            //AirCommando a1 = new AirCommando("itzik", "231", " ");
+            //SeaCommando s1 = new SeaCommando("chaim", "===", " ");
+            //Commando [] nn = { c1, a1, s1 };
+            //foreach (var i in nn)
+            //{
+            //    Console.WriteLine(i.GetType());
+            //    i.Attack();
+            //}  
+            EnemyFactory.GetEnemy(5);
+            foreach (Enemy name in EnemyFactory.GetListEnemy())
             {
-                Console.WriteLine(i.GetType());
-                i.Attack();
-            }  
+                name.Shout();
+            }
 
 
 
