863fd1d75eef25c8d193d93c534cfb853989ec16
yitzchak shaish <y350380350@gmail.com>
2025-05-28 12:37:37 +0300

    Fixsed Program

--- Program.cs
+++ Program.cs
@@ -4,32 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CommandoPproject
+namespace CommandoProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //Commando c1= new Commando("moshe","xxx", "Hide");
-            //c1.Walk();
-            //c1.SayName("COLONEL");
-            //Weapon ww = new Weapon("gon","refael",3);
-            //AirCommando a1 = new AirCommando("itzik", "231", " ");
-            //SeaCommando s1 = new SeaCommando("chaim", "===", " ");
-            //Commando [] nn = { c1, a1, s1 };
-            //foreach (var i in nn)
-            //{
-            //    Console.WriteLine(i.GetType());
-            //    i.Attack();
-            //}  
-            EnemyFactory.GetEnemy(5);
-            foreach (Enemy name in EnemyFactory.GetListEnemy())
-            {
-                name.Shout();
-            }
+        
+                Game game = new Game();
+                game.Initialize();
+                game.PrintSummary();
 
-
-
+            
 
 
         }
