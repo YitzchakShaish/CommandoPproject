0b00c430c32bbeae14e0ce428735ecc4959ae097
yitzchak shaish <y350380350@gmail.com>
2025-05-29 11:19:19 +0300

    Added a Commando folder. Putting all Commando departments into a folder.

---
+++ Commandors/SeaCommando .cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CommandoProject
+{
+    internal class SeaCommando : Commando
+    {
+
+        public SeaCommando(string name, string codeName) : base(name, codeName)
+        {
+        }
+        public void Whoa(int meter)
+        {
+            Console.WriteLine($"The soldier swims {meter} meters");
+        }
+        public override void Attack()
+        {
+            Console.WriteLine($" Teh \"sea Commando\" with Code Name: \"{CodeName}\" is active.");
+        }
+    }
+}
+
+
