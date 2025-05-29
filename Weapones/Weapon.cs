dfcaaf197f8c611b0572e426ffdc5ef1fe1e64ad
yitzchak shaish <y350380350@gmail.com>
2025-05-29 11:21:50 +0300

    Added a folder Weapons. Moved an existing class named Weapons to the folder. Added classes for "Stone" and "Knife"

---
+++ Weapones/Weapon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CommandoProject
+{
+    internal class Weapon
+    {
+        string Name;
+        string ManuFacturer;
+        int NumberPalls;
+    
+    public Weapon(string name, string manuFacturer, int numberPalls)
+        {
+            Name = name;
+            ManuFacturer = manuFacturer;
+            NumberPalls = numberPalls;
+
+        }
+    public void Shoot()
+        {
+            if (NumberPalls > 0)
+            {
+                Console.WriteLine("One bullet was fired");
+                NumberPalls = NumberPalls-1;
+            }
+            else Console.WriteLine("There is no bullet in the cartridge to fire.");
+        }
+
+    }
+}
