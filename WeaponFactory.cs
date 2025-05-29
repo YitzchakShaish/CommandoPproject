dc02545910cffc8990daa5fdc907ef59389c156b
yitzchak shaish <y350380350@gmail.com>
2025-05-29 13:06:31 +0300

    Added the ColdWeaponFactory

--- WeaponFactory.cs
+++ WeaponFactory.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommandoProject.Interfaces;
+using CommandoProject.Weapones;
 
 namespace CommandoProject
 {
@@ -16,6 +18,30 @@ namespace CommandoProject
         public List<Weapon> GetListWeapon()
         {
             return listWeapon;
+        }
+
+
+        private List<IColdWeapon> listColdWeapon = new List<IColdWeapon>();
+
+        public void GetColdWeapon(string tipe ,string name, string color)
+        {
+            switch (tipe)
+            {
+                case "knife":
+                    listColdWeapon.Add(new Knife (name, color));
+                    break;
+                case "stone":
+                    listColdWeapon.Add(new Stone(name, color));
+                    break;
+                default:
+                    Console.WriteLine("Your type selection is invalid.");
+                    break;
+
+            }
+        }
+        public List<IColdWeapon> GetListColdWeapon()
+        {
+            return listColdWeapon;
         }
     }
 }
